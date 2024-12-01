a5ff3c2dbc9a9605dc24f9c207b568c4dc05e82a
Anthony Lima <anthcostalima@gmail.com>
2024-12-01 14:01:39 -0300

    exchange model and adding frontend preview

--- index.py
+++ index.py
@@ -1,24 +1,29 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
-from transformers import AutoProcessor, BlipForConditionalGeneration
-from PIL import Image
+from transformers import BlipProcessor, BlipForConditionalGeneration
+from PIL import Image, ImageDraw
+import torch
 import io
+import base64
 
 app = FastAPI()
-
-processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
-model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large").to("cuda")
-@app.post("/classificar-imagem/")
-async def classificar_imagem(file: UploadFile = File(...)):
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"], 
+)
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+@app.post("/generatetext")
+async def GenerateTextForInstagram(file: UploadFile = File(...)):
     try:
-        # Leia a imagem
         image_data = await file.read()
         img = Image.open(io.BytesIO(image_data)).convert("RGB")
-        inputs = processor(img, return_tensors="pt").to("cuda")
-        
+        inputs = processor(img, return_tensors="pt");
         out = model.generate(**inputs)
-        
-        results = processor.decode(out[0], skip_special_tokens=True)
-        return JSONResponse(content=results)
+        return JSONResponse(content={"text":processor.decode(out[0], skip_special_tokens=True)})
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
